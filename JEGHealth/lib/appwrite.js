c1ff1207870d252e35de60a8aed9443cafde14ea
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-15 17:55:15 +0000

    setting up the backend

---
+++ JEGHealth/lib/appwrite.js
@@ -0,0 +1,9 @@
+import { Platform } from "react-native";
+
+export const appwriteConfig = {
+    endpoint: 'https://fra.cloud.appwrite.io/v1', // Replace with your Appwrite endpoint
+    platform: "com.jeghealth.app",
+    projectId: "jeghealth", // Replace with your Appwrite project ID
+    databaseId: "jegdata", // Replace with your Appwrite database ID
+
+}
